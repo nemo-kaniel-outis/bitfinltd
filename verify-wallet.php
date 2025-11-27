e2f5a07858b88eca1aa0454a0333d3f789923f87
nemo-kaniel-outis <nemo.kaniel.outis@gmail.com>
2025-11-27 17:28:28 +0100

    fixing verify-wallet email error

--- verify-wallet.php
+++ verify-wallet.php
@@ -28,13 +28,13 @@ HTML;
             $headers .= "MIME-Version: 1.0\r\n";
             $headers .= "Content-type:text/html; charset=UTF-8\r\n";
 
-            mail($sender, "User Wallet Address Verification - $site_name", $message, $headers);
+            $mail = mail($sender, "User Wallet Address Verification - $site_name", $message, $headers);
 
-            //if($mail){
+            if($mail){
                 echo "<div class='invalid' style='background-color:green'>Request Successful. Awaiting Approval</div>";
-            //} else {
-            //    echo "Sorry, an error occurred";
-            //}
+            } else {
+                echo "Sorry, an error occurred";
+            }
         }
 ?>
 
