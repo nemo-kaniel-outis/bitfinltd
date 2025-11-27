02be0488c1615df5f4cd2cedef765b4d12365e56
nemo-kaniel-outis <nemo.kaniel.outis@gmail.com>
2025-11-27 16:16:17 +0100

    testing out new site and webhooks

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
-echo "Hello World!!";
+include_once($_SERVER["DOCUMENT_ROOT"]."/views/Index_Segments.php");
+//include_once($_SERVER["DOCUMENT_ROOT"]."/views/Index_2Segments.php");
 
+Index_Segments::header();
+//Index_Segments::trunk();
+//Index_Segments::side();  
+//Index_Segments::footer();
 ?>
